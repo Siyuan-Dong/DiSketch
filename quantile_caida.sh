a19cfbdfafa6cb8387b6ed866d01d3dc903083e9
Siyuan-Dong <113673486+Siyuan-Dong@users.noreply.github.com>
2023-03-28 22:56:44 -0700

    update codes

---
+++ quantile_caida.sh
@@ -0,0 +1,48 @@
+make
+# test4 large
+./main 100 4 10 caida
+# ./main 300 4 10 caida
+# ./main 500 4 10 caida
+# ./main 700 4 10 caida
+# ./main 900 4 10 caida
+# ./main 1100 4 10 caida
+# ./main 1300 4 10 caida
+
+
+# test5 median
+./main 100 5 10 caida
+# ./main 300 5 10 caida
+# ./main 500 5 10 caida
+# ./main 700 5 10 caida
+# ./main 900 5 10 caida
+# ./main 1100 5 10 caida
+# ./main 1300 5 10 caida
+
+
+# test1 tiny
+./main 10000 1 10 caida
+
+# test2 insert
+./main 200 2 10 caida
+# ./main 400 2 10 caida
+# ./main 600 2 10 caida
+# ./main 800 2 10 caida
+# ./main 1000 2 10 caida
+# ./main 1200 2 10 caida
+# ./main 1400 2 10 caida
+# ./main 1600 2 10 caida
+# ./main 1800 2 10 caida
+# ./main 2000 2 10 caida
+
+# test3 query
+./main 200 3 10 caida
+# ./main 400 3 10 caida
+# ./main 600 3 10 caida
+# ./main 800 3 10 caida
+# ./main 1000 3 10 caida
+# ./main 1200 3 10 caida
+# ./main 1400 3 10 caida
+# ./main 1600 3 10 caida
+# ./main 1800 3 10 caida
+# ./main 2000 3 10 caida
+
